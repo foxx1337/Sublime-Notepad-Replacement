fcc6ba8b0d6ab341cbf8638372bf37510443c2c5
foxx1337 <foxx1337@yahoo.com>
2019-01-05 13:58:29 +0100

    Simplify and uniformize code

--- setup.ps1
+++ setup.ps1
@@ -1,11 +1,21 @@
 Param (
-    [parameter(HelpMessage="Full path to Sublime Text (sublime_text.exe)")]
+    [Parameter(
+        HelpMessage="Full path to Sublime Text (sublime_text.exe)."
+    )]
     [alias("s", "st", "subl", "sublime")]
+    [ValidateScript({
+        Test-Path -Path $_ -PathType Leaf
+    })]
     [String]
     $SublimePath = $(Join-Path (Get-Location).Path "sublime_text.exe"),
     
-    [parameter(HelpMessage="Full path to the Sublime Notepad replacement launcher ")]
+    [Parameter(
+        HelpMessage="Full path to the Sublime Notepad replacement launcher."
+    )]
     [alias("l", "launcher")]
+    [ValidateScript({
+        Test-Path -Path $_ -PathType Leaf
+    })]
     [String]
     $LauncherPath = $(Join-Path (Get-Location).Path "SublimeLauncher.exe"),
 
@@ -23,17 +33,7 @@ else
     $LauncherPath = (Resolve-Path $LauncherPath)
     $SublimePath = (Resolve-Path $SublimePath)
 
-    if (!(Test-Path $LauncherPath -PathType Leaf))
-    {
-        Write-Output "Cannot find the SublimeLauncher.exe path at ""$LauncherPath"". Specify it with the -l flag."
-        Exit 1
-    }
-
-    if (!(Test-Path $SublimePath -PathType Leaf))
-    {
-        Write-Output "Cannot find the sublime_text.exe path at ""$SublimePath"". Specify it with the -s flag."
-        Exit 1
-    }
+    Write-Output "wtf got $LauncherPath and $SublimePath"
 
     New-Item "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\notepad.exe" -ErrorAction SilentlyContinue
     New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\notepad.exe" -Name "Debugger" -Value """$LauncherPath"" -s ""$SublimePath"" -z" -ErrorAction SilentlyContinue
--- src/Sublime Launcher/SublimeLauncher/SublimeLauncher.cpp
+++ src/Sublime Launcher/SublimeLauncher/SublimeLauncher.cpp
@@ -15,7 +15,7 @@ int APIENTRY _tWinMain(HINSTANCE hInstance,
     UNREFERENCED_PARAMETER(hPrevInstance);
 
     const size_t length = wcslen(lpCmdLine) + 2;
-    LPTSTR arguments = new TCHAR[length];
+    const LPTSTR arguments = new TCHAR[length];
     arguments[0] = L'\0';
 
     int argc;
@@ -23,7 +23,7 @@ int APIENTRY _tWinMain(HINSTANCE hInstance,
 
     bool argumentsPassed = false;
 
-	wchar_t *sublimeExe = nullptr;
+    wchar_t *sublimeExe = nullptr;
 
     for (int i = 1; i < argc; i++)
     {
@@ -32,43 +32,47 @@ int APIENTRY _tWinMain(HINSTANCE hInstance,
             // -z specified - skip the next parameter
             i++;
         }
-		else if (wcscmp(wargv[i], L"-s") == 0)
-		{
-			// -s specified - the next parameter is the full path to sublime_text.exe
-			i++;
-			if (i >= argc)
-			{
-				break;
-			}
+        else if (wcscmp(wargv[i], L"-s") == 0)
+        {
+            // -s specified - the next parameter is the full path to sublime_text.exe
+            i++;
+            if (i >= argc)
+            {
+                break;
+            }
 
-			sublimeExe = _wcsdup(wargv[i]);
-		}
+            sublimeExe = _wcsdup(wargv[i]);
+        }
         else
         {
-            if (argumentsPassed == false)
+            if (!argumentsPassed)
+            {
                 wcscat_s(arguments, length, L"\"");
+            }
             else
+            {
                 wcscat_s(arguments, length, L" ");
+            }
 
             argumentsPassed = true;
             wcscat_s(arguments, length, wargv[i]);
         }
     }
 
-    if (argumentsPassed == true)
+    if (argumentsPassed)
+    {
         wcscat_s(arguments, length, L"\"");
-    // this is a test
-    // and another
+    }
     
     if (sublimeExe == nullptr)
-	{
-		sublimeExe = _wcsdup(wargv[0]);
-		const int lengthExe = wcslen(sublimeExe) + 1;
-		sublimeExe[wcslen(sublimeExe) - wcslen(L"SublimeLauncher.exe")] = '\0';
-		wcscat_s(sublimeExe, lengthExe, L"sublime_text.exe");
-	}
+    {
+        sublimeExe = _wcsdup(wargv[0]);
+        const int lengthExe = wcslen(sublimeExe) + 1;
+        sublimeExe[wcslen(sublimeExe) - wcslen(L"SublimeLauncher.exe")] = '\0';
+        wcscat_s(sublimeExe, lengthExe, L"sublime_text.exe");
+    }
 
-    ShellExecute(NULL, NULL, sublimeExe, arguments, NULL, nCmdShow);
+    ShellExecute(nullptr, nullptr, sublimeExe, arguments, nullptr, nCmdShow);
 
     return (int) 0;
 }
